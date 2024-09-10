816026bf80eff56abefddcd32762a3e662ecb914
Cristobal  Camarena Hernández <cristobalcamarena@MacBook-Air-de-Cristobal-2.local>
2024-09-10 09:38:33 -0600

    ACTUALIZADO

--- Paint.py
+++ Paint.py
@@ -1,14 +1,3 @@
-"""Paint, for drawing shapes.
-
-Exercises
-
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
-"""
-
 from turtle import *
 
 from freegames import vector
