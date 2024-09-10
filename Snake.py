62cfcdf20c6ecd1a1955b7c1a271ea0cc8bd520a
Cristobal  Camarena Hernández <cristobalcamarena@MacBook-Air-de-Cristobal-2.local>
2024-09-10 07:08:47 -0600

    Juego Snake

---
+++ Snake.py
@@ -0,0 +1,83 @@
+from random import randrange, choice
+from turtle import *
+from freegames import square, vector
+
+# Posibles colores excluyendo el rojo
+colors = ['blue', 'green', 'yellow', 'purple', 'orange']
+
+# Inicializa comida y serpiente
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
+
+# Selecciona colores aleatorios para la serpiente y la comida, asegurando que sean diferentes
+snake_color = choice(colors)
+food_color = choice([color for color in colors if color != snake_color])
+
+def change(x, y):
+    """Cambia la dirección de la serpiente."""
+    aim.x = x
+    aim.y = y
+
+def inside(head):
+    """Devuelve True si la cabeza está dentro de los límites."""
+    return -200 < head.x < 190 and -200 < head.y < 190
+
+def move_food():
+    """Mueve la comida un paso al azar dentro de los límites."""
+    directions = [vector(10, 0), vector(-10, 0), vector(0, 10), vector(0, -10)]
+    move_direction = choice(directions)
+    new_position = food + move_direction
+
+    # Asegura que la comida no salga de los límites
+    if inside(new_position):
+        food.move(move_direction)
+
+def move():
+    """Mueve la serpiente un segmento hacia adelante."""
+    head = snake[-1].copy()
+    head.move(aim)
+
+    if not inside(head) or head in snake:
+        square(head.x, head.y, 9, 'red')  # Marca la serpiente como colisionada
+        update()
+        return
+
+    snake.append(head)
+
+    if head == food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+    else:
+        snake.pop(0)
+
+    move_food()  # Mueve la comida un paso aleatorio
+
+    clear()
+
+    # Dibuja la serpiente
+    for body in snake:
+        square(body.x, body.y, 9, snake_color)
+
+    # Dibuja la comida
+    square(food.x, food.y, 9, food_color)
+    
+    update()
+    ontimer(move, 100)
+
+# Configuración inicial del juego
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+
+# Controles de dirección de la serpiente
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
+
+# Inicia el juego
+move()
+done()
